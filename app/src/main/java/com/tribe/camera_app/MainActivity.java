f542be4fa6dc2f97803519bdc022c970f2941b92
aniketmund21 <aniiketmund53@gmail.com>
2021-08-29 21:31:08 +0530

    Added first screen

--- app/src/main/java/com/tribe/camera_app/MainActivity.java
+++ app/src/main/java/com/tribe/camera_app/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.tribe.camera_app.Utils.Utils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Utils.blackIconStatusBar(MainActivity.this, R.color.background_light);
     }
 }
